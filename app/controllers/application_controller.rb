c6dc2eb9143606f992f0267100860257496c1e3a
jiri <jiribruk@seznam.cz>
2025-05-21 22:07:22 +0200

    added quiz_controller with index and show +spec. rescue not found for application_controller.rb. routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  rescue_from(::ActiveRecord::RecordNotFound) { |exception| render_404(exception) }
+
+
+  def render_404(exception)
+    render plain: exception.message, status: 404
+  end
+
 end
