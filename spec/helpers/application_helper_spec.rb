ab383f52344450353a798e24dbdeb4b54e3a39c2
jiri bruk <jiri.bruk@easysoftware.com>
2025-05-24 18:32:52 +0200

    navbar with spec

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -5,7 +5,23 @@ require 'rails_helper'
 describe ApplicationHelper, type: :helper do
   describe '#heading' do
     it 'renders an h1 tag with the given text and class' do
-      expect(helper.heading(text: 'Test')).to eq('<h1 class="mb-4">Test</h1>')
+      expect(heading(text: 'Test')).to eq('<h1 class="mb-4">Test</h1>')
+    end
+  end
+
+  describe '#top_navbar' do
+    subject(:navbar) { top_navbar }
+
+    it 'renders a nav element with the correct classes' do
+      expect(navbar).to have_selector('nav.navbar.border-bottom')
+    end
+
+    it 'contains a link to the home page' do
+      expect(navbar).to have_link('Home', href: root_path)
+    end
+
+    it 'applies the correct background style' do
+      expect(navbar).to have_selector('nav[style*="#FFA500"]')
     end
   end
 end
