8ec89e0180bc40773d2c5c182a33e113edb600fa
jiri <jiribruk@seznam.cz>
2025-07-04 23:05:27 +0200

    Image for question add and show

--- app/javascript/controllers/quizzes_controller.js
+++ app/javascript/controllers/quizzes_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["submit"]
+  static targets = ["submit", "destroyField", "preview", "fileInput"]
 
   connect() {
     this.submitTargets.forEach(button => {
@@ -19,14 +19,26 @@ export default class extends Controller {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
 
-  static targets = ["destroyField"]
-
   remove() {
     if (this.hasDestroyFieldTarget) {
       this.destroyFieldTarget.value = "1"
     }
 
     this.element.style.display = "none"
+  }
+
+  previewImage(event) {
+    const input = event.target
+    const file = input.files[0]
+    if (!file) return
+
+    const reader = new FileReader()
+    reader.onload = (e) => {
+      const previewEl = this.previewTarget
+      previewEl.src = e.target.result
+      previewEl.classList.remove("d-none")
+    }
+    reader.readAsDataURL(file)
   }
 
 }
