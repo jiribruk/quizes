05da461ecb51a22aaaa426b121f7e832ca559de5
Jiri Bruk <jiri.jiri.bruk@gmail.com>
2025-09-05 23:13:38 +0200

    top bar polish styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,35 +13,35 @@ module ApplicationHelper
   def top_navbar
     tag.nav(class: 'navbar fixed-top border-bottom', style: 'background-color: #FFA500;') do
       tag.div(class: 'container-fluid') do
-        tag.a(class: 'navbar-brand', href: root_path) do
-          t('buttons.home')
+        # Left side - Home, New Quiz and My Groups buttons
+        tag.div(class: 'd-flex align-items-center') do
+          tag.a(t('buttons.home'), class: 'btn btn-sm me-3 navbar-btn', href: root_path) +
+          (user_signed_in? ? 
+            tag.a(t('buttons.new_quiz'), class: 'btn btn-sm me-3 navbar-btn', href: new_quiz_path) +
+            tag.a(t('user_groups.index.title'), class: 'btn btn-sm me-3 navbar-btn', href: user_groups_path) : 
+            '')
         end +
-                  tag.div(class: 'navbar-nav ms-auto') do
+        # Right side - User controls
+        tag.div(class: 'navbar-nav ms-auto') do
           if user_signed_in?
-            tag.div(class: 'navbar-nav align-items-center') do
-              tag.div(class: 'd-flex align-items-center me-3') do
-                tag.img(src: current_user.gravatar_url(size: 32), 
-                        class: 'rounded-circle me-2', 
-                        alt: current_user.display_name,
-                        style: 'width: 32px; height: 32px;',
-                        title: t('users.navbar.signed_in_as', name: current_user.display_name))
-              end +
-              tag.div(class: 'd-flex') do
-                tag.a(t('user_groups.index.title'), class: 'btn btn-outline-dark btn-sm me-2', 
-                      href: user_groups_path) +
-                tag.a(t('users.navbar.edit_profile'), class: 'btn btn-outline-dark btn-sm me-2', 
-                      href: edit_user_registration_path) +
-                tag.a(t('devise.sessions.signed_out'), class: 'btn btn-outline-dark btn-sm', 
-                      href: destroy_user_session_path, 
-                      data: { turbo_method: :delete, 
-                              turbo_confirm: t('users.navbar.confirm_sign_out') })
-              end
+            tag.div(class: 'd-flex align-items-center') do
+              tag.img(src: current_user.gravatar_url(size: 32), 
+                      class: 'rounded-circle me-2 navbar-gravatar', 
+                      alt: current_user.display_name,
+                      style: 'width: 32px; height: 32px;',
+                      title: t('users.navbar.signed_in_as', name: current_user.display_name)) +
+              tag.a(t('users.navbar.edit_profile'), class: 'btn btn-sm me-2 navbar-btn', 
+                    href: edit_user_registration_path) +
+              tag.a(t('devise.sessions.signed_out'), class: 'btn btn-sm navbar-btn-logout', 
+                    href: destroy_user_session_path, 
+                    data: { turbo_method: :delete, 
+                            turbo_confirm: t('users.navbar.confirm_sign_out') })
             end
           else
             tag.div(class: 'd-flex') do
-              tag.a(t('devise.shared.links.sign_in'), class: 'btn btn-outline-dark btn-sm me-2', 
+              tag.a(t('devise.shared.links.sign_in'), class: 'btn btn-sm me-2 navbar-btn', 
                     href: new_user_session_path) +
-              tag.a(t('devise.shared.links.sign_up'), class: 'btn btn-dark btn-sm', 
+              tag.a(t('devise.shared.links.sign_up'), class: 'btn btn-sm navbar-btn-signup', 
                     href: new_user_registration_path)
             end
           end
